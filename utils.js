aa1ce90507a45968e739d00a2e17bd00f11083d6
JoeD <49292088+RedRob0@users.noreply.github.com>
2019-04-25 14:14:22 -0700

    Update utils.js

--- utils.js
+++ utils.js
@@ -7,10 +7,18 @@ module.exports.getDb = function() {
 };
 
 module.exports.init = function() {
-    MongoClient.connect('mongodb://localhost:27017/test', function(err, client) {
-        if (err)
-            console.log('unable to connect to DB');
-        else
+    // MongoClient.connect('mongodb://localhost:27017/test', function(err, client) {
+    //     if (err)
+    //         console.log('unable to connect to DB');
+    //     else
+    //         _db = client.db('test');
+    // });
+    MongoClient.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/forum", function (err, client) {
+        if (err) {
+            console.log(err);
+            process.exit(1);
+        } else {
             _db = client.db('test');
+        }
     });
 };
